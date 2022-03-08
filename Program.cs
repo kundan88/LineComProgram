8aa45e9c5f382c8e0530c078aeec033f880a8449
Kundan <kundan kamble>
2022-03-08 06:50:52 -0800

    Line comparison

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace lineprogram;
+public class program
+{
+
+
+    static void Main(string[] args)
+    {
+        double x1 = 1;
+        double y1 = 1;
+        double x2 = 4;
+        double y2 = 4;
+        double len = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        Console.WriteLine("Length between two line" + len);
+
+    }
+}
